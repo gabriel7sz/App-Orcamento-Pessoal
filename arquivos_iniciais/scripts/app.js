450e98c0546f4b9549adb79e6a137b2eeafb7c88
gabriel7sz <gabriel7sz@outlook.com>
2025-02-17 14:48:59 -0300

    criando lógica para mostrar na tabela dá página consulta.html de forma programática

--- arquivos_iniciais/scripts/app.js
+++ arquivos_iniciais/scripts/app.js
@@ -122,6 +122,44 @@ function carregaListaDespesas() {
 
     despesas = bd.recuperarTodosRegistros()
 
-    console.log(despesas);
-    
-}
+    //selecionando o elemento tbody da tabela   
+    let listaDespesas = document.getElementById('listaDespesas')
+    /*
+        <tr>
+            <td>11/02/2025</td>
+            <td>Educação</td>
+            <td>Curso Prog</td>
+            <td>170,00</td>
+        </tr>
+    */ 
+
+    //percorrer o array despesas, listando cada despesa de forma dinamica
+    despesas.forEach(function(d) {
+
+        console.log(d);
+
+        //criando as linhas
+        let linha = listaDespesas.insertRow()
+
+        //criar as colunas
+        linha.insertCell(0).innerHTML =  `${d.dia}/${d.mes}/${d.ano}`
+
+        //ajustar o tipo 
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+            break
+            case '2': d.tipo = 'Educação'
+            break
+            case '3': d.tipo = 'Lazer'
+            break
+            case '4': d.tipo = 'Saúde'
+            break
+            case '5': d.tipo = 'Transporte'
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+
+        linha.insertCell(2).innerHTML = d.descricao 
+        linha.insertCell(3).innerHTML = d.valor
+    })
+}   
+
